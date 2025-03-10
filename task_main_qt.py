88011b3fa01f504eb1552051930e5948d2d97c6f
bsherin <bsherin@northwestern.edu>
2025-03-10 18:35:50 -0500

    Clear button Made datearea class reordered

--- task_main_qt.py
+++ task_main_qt.py
@@ -21,6 +21,36 @@ projects = [
     "CS+LS"
 ]
 
+class DateArea(QWidget):
+    def __init__(self, name, parent_layout, form_widget):
+        super().__init__()
+        self.form_widget = form_widget
+        self.date_label = QLabel(f"{name}:")
+        parent_layout.addWidget(self.date_label)
+
+        layout = QHBoxLayout()
+        self.date_entry = QLineEdit()
+        self.date_entry.setPlaceholderText("YYYY-MM-DD")
+        layout.addWidget(self.date_entry)
+
+        self.calendar_button = QPushButton("📅")
+        self.calendar_button.clicked.connect(self.show_calendar)
+        layout.addWidget(self.calendar_button)
+
+        parent_layout.addLayout(layout)
+
+    def get_date(self):
+        return self.date_entry.text()
+
+    def set_date(self, date):
+        self.date_entry.setText(date)
+
+    def show_calendar(self):
+        dialog = CalendarDialog(self, self.form_widget)
+        if dialog.exec():  # This is now valid - QDialog has exec()
+            selected_date = dialog.selected_date
+            self.date_entry.setText(selected_date.toString(QtCore.Qt.DateFormat.ISODate))
+
 
 class ProjectForm(QWidget):
     def __init__(self):
@@ -38,39 +68,6 @@ class ProjectForm(QWidget):
         layout.addWidget(self.name_label)
         layout.addWidget(self.name_entry)
 
-        # Start Date field with calendar picker
-        self.date_label = QLabel("Start Date:")
-        layout.addWidget(self.date_label)
-
-        date_layout = QHBoxLayout()
-        self.date_entry = QLineEdit()
-        self.date_entry.setPlaceholderText("YYYY-MM-DD")
-        # self.date_entry.setText(QDate.currentDate().toString(QtCore.Qt.DateFormat.ISODate))
-
-        self.calendar_button = QPushButton("📅")
-        self.calendar_button.clicked.connect(self.show_calendar)
-
-        date_layout.addWidget(self.date_entry)
-        date_layout.addWidget(self.calendar_button)
-
-        layout.addLayout(date_layout)
-
-        # Due Date field with calendar picker
-        self.due_date_label = QLabel("Due Date:")
-        layout.addWidget(self.due_date_label)
-
-        due_date_layout = QHBoxLayout()
-        self.due_date_entry = QLineEdit()
-        self.due_date_entry.setPlaceholderText("YYYY-MM-DD")
-
-        self.due_calendar_button = QPushButton("📅")
-        self.due_calendar_button.clicked.connect(self.show_due_calendar)
-
-        due_date_layout.addWidget(self.due_date_entry)
-        due_date_layout.addWidget(self.due_calendar_button)
-
-        layout.addLayout(due_date_layout)
-
         # Project dropdown
         self.project_label = QLabel("Project:")
         self.project_dropdown = QComboBox()
@@ -78,10 +75,19 @@ class ProjectForm(QWidget):
         layout.addWidget(self.project_label)
         layout.addWidget(self.project_dropdown)
 
+        self.start_date_area = DateArea("Start Date", layout, self)
+        self.due_date_area = DateArea("Due Date", layout, self)
+
+        button_layout = QHBoxLayout()
         # Submit button
         self.submit_button = QPushButton("Submit")
         self.submit_button.clicked.connect(self.submit_form)
-        layout.addWidget(self.submit_button)
+        button_layout.addWidget(self.submit_button)
+
+        self.clear_button = QPushButton("Clear")
+        self.clear_button.clicked.connect(self.clear_form)
+        button_layout.addWidget(self.clear_button)
+        layout.addLayout(button_layout)
 
         self.setLayout(layout)
 
@@ -94,22 +100,16 @@ class ProjectForm(QWidget):
 
         self.move(x, y)
 
-    def show_calendar(self):
-        dialog = CalendarDialog(self)
-        if dialog.exec():  # This is now valid - QDialog has exec()
-            selected_date = dialog.selected_date
-            self.date_entry.setText(selected_date.toString(QtCore.Qt.DateFormat.ISODate))
-
-    def show_due_calendar(self):
-        dialog = CalendarDialog(self)
-        if dialog.exec():  # This is now valid - QDialog has exec()
-            selected_date = dialog.selected_date
-            self.due_date_entry.setText(selected_date.toString(QtCore.Qt.DateFormat.ISODate))
+    def clear_form(self):
+        self.name_entry.clear()
+        self.start_date_area.set_date("")
+        self.due_date_area.set_date("")
+        self.project_dropdown.setCurrentIndex(0)
 
     def submit_form(self):
         name = self.name_entry.text()
-        start_date = self.date_entry.text()
-        due_date = self.due_date_entry.text()
+        start_date = self.start_date_area.get_date()
+        due_date = self.due_date_area.get_date()
         project = self.project_dropdown.currentText()
 
         if not name:
@@ -124,7 +124,7 @@ class ProjectForm(QWidget):
 
 
 class CalendarDialog(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, parent, form_widget):
         super().__init__(parent)
 
         self.setWindowTitle("Select Date")
@@ -143,7 +143,7 @@ class CalendarDialog(QDialog):
 
         # Connect the calendar click signal
         self.calendar.clicked.connect(self.date_selected)
-        parent_pos = parent.mapToGlobal(parent.date_entry.pos())
+        parent_pos = form_widget.mapToGlobal(parent.date_entry.pos())
         self.move(parent_pos.x(), parent_pos.y() + 30)
 
     def date_selected(self, date):
